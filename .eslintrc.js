7bb2009de987a1c320f151d5ff31be0fbb608cb6
gianlucastrada <gianluca.strada@alten.it>
2024-01-04 02:55:01 +0100

    chore(lock): removed es lint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,16 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  // plugins: ['@typescript-eslint', 'simple-import-sort', 'unused-imports'],
+  plugins: [
+    // '@typescript-eslint',
+    // 'simple-import-sort',
+    // 'unused-imports'
+  ],
   extends: [
     'eslint:recommended',
     'next',
     'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
+    // 'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
   rules: {
@@ -26,55 +30,55 @@ module.exports = {
 
     //#region  //*=========== Unused Import ===========
     // '@typescript-eslint/no-unused-vars': 'off',
-    'unused-imports/no-unused-imports': 'warn',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        varsIgnorePattern: '^_',
-        args: 'after-used',
-        argsIgnorePattern: '^_',
-      },
-    ],
+    // 'unused-imports/no-unused-imports': 'warn',
+    // 'unused-imports/no-unused-vars': [
+    //   'warn',
+    //   {
+    //     vars: 'all',
+    //     varsIgnorePattern: '^_',
+    //     args: 'after-used',
+    //     argsIgnorePattern: '^_',
+    //   },
+    // ],
     //#endregion  //*======== Unused Import ===========
 
     //#region  //*=========== Import Sort ===========
-    'simple-import-sort/exports': 'warn',
-    'simple-import-sort/imports': [
-      'warn',
-      {
-        groups: [
-          // ext library & side effect imports
-          ['^@?\\w', '^\\u0000'],
-          // {s}css files
-          ['^.+\\.s?css$'],
-          // Lib and hooks
-          ['^@/lib', '^@/hooks'],
-          // static data
-          ['^@/data'],
-          // components
-          ['^@/components', '^@/container'],
-          // zustand store
-          ['^@/store'],
-          // Other imports
-          ['^@/'],
-          // relative paths up until 3 level
-          [
-            '^\\./?$',
-            '^\\.(?!/?$)',
-            '^\\.\\./?$',
-            '^\\.\\.(?!/?$)',
-            '^\\.\\./\\.\\./?$',
-            '^\\.\\./\\.\\.(?!/?$)',
-            '^\\.\\./\\.\\./\\.\\./?$',
-            '^\\.\\./\\.\\./\\.\\.(?!/?$)',
-          ],
-          ['^@/types'],
-          // other that didnt fit in
-          ['^'],
-        ],
-      },
-    ],
+    // 'simple-import-sort/exports': 'warn',
+    // 'simple-import-sort/imports': [
+    //   'warn',
+    //   {
+    //     groups: [
+    //       // ext library & side effect imports
+    //       ['^@?\\w', '^\\u0000'],
+    //       // {s}css files
+    //       ['^.+\\.s?css$'],
+    //       // Lib and hooks
+    //       ['^@/lib', '^@/hooks'],
+    //       // static data
+    //       ['^@/data'],
+    //       // components
+    //       ['^@/components', '^@/container'],
+    //       // zustand store
+    //       ['^@/store'],
+    //       // Other imports
+    //       ['^@/'],
+    //       // relative paths up until 3 level
+    //       [
+    //         '^\\./?$',
+    //         '^\\.(?!/?$)',
+    //         '^\\.\\./?$',
+    //         '^\\.\\.(?!/?$)',
+    //         '^\\.\\./\\.\\./?$',
+    //         '^\\.\\./\\.\\.(?!/?$)',
+    //         '^\\.\\./\\.\\./\\.\\./?$',
+    //         '^\\.\\./\\.\\./\\.\\.(?!/?$)',
+    //       ],
+    //       ['^@/types'],
+    //       // other that didnt fit in
+    //       ['^'],
+    //     ],
+    //   },
+    // ],
     //#endregion  //*======== Import Sort ===========
   },
   globals: {
