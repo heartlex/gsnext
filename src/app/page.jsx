cf9b2df3cede481a229202e95ffd3abeccbee728
gianlucastrada <gianluca.strada@alten.it>
2024-01-30 01:32:34 +0100

    fix(home): button resume

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -40,12 +40,13 @@ function Techs() {
   return (
     <div className='mt-24 rounded-b-4xl bg-neutral-950 py-20 sm:mt-32 sm:py-32 lg:mt-56'>
       <Container>
-        <FadeIn className='flex items-center gap-x-8'>
+        <FadeIn className='flex flex-wrap items-center gap-x-8'>
           <h2 className='text-center font-display text-sm font-semibold tracking-wider text-white sm:text-left'>
             I work with such amazing tools
           </h2>
           <div className='h-px flex-auto bg-neutral-800' />
           <Button
+            className='my-6 sm:my-0'
             href='https://www.dropbox.com/s/7b8i0paenrfg7qx/cv_EN.pdf?dl=0'
             download
             aria-label='View resume'
