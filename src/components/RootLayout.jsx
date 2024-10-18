9942b6e890e217e8d90f342fbbbc25891ce97373
gianlucastrada <G.Strada@contractors.crifgroup.com>
2024-10-19 00:30:23 +0200

    fix(logo): fixed behavior in home

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -39,10 +39,16 @@ function Header({
   onToggle,
   toggleRef,
 }) {
+  const handleInHomeClick = (e) => {
+    if (window.location.pathname === '/') {
+      e.preventDefault(); // Prevent the default navigation
+    }
+  };
+
   return (
     <Container>
       <div className='flex items-center justify-between'>
-        <Link href='/' aria-label='Home'>
+        <Link href='/' aria-label='Home' onClick={handleInHomeClick}>
           <GSLogo className='h-24' invert={invert} animated={true} />
         </Link>
         <div className='flex items-center gap-x-8'>
@@ -54,7 +60,7 @@ function Header({
             aria-controls={panelId}
             className={clsx(
               'group -m-2.5 rounded-full p-2.5 transition',
-              invert ? 'hover:bg-white/10' : 'hover:bg-neutral-950/10'
+              invert ? 'hover:bg-white/10' : 'hover:bg-neutral-950/10',
             )}
             aria-label='Toggle navigation'
           >
@@ -63,7 +69,7 @@ function Header({
                 'h-6 w-6',
                 invert
                   ? 'fill-white group-hover:fill-neutral-200'
-                  : 'fill-neutral-950 group-hover:fill-neutral-700'
+                  : 'fill-neutral-950 group-hover:fill-neutral-700',
               )}
             />
           </button>
@@ -143,7 +149,7 @@ function RootLayoutInner({ children }) {
             onToggle={() => {
               setExpanded((expanded) => !expanded);
               window.setTimeout(() =>
-                closeRef.current?.focus({ preventScroll: true })
+                closeRef.current?.focus({ preventScroll: true }),
               );
             }}
           />
@@ -168,7 +174,7 @@ function RootLayoutInner({ children }) {
                 onToggle={() => {
                   setExpanded((expanded) => !expanded);
                   window.setTimeout(() =>
-                    openRef.current?.focus({ preventScroll: true })
+                    openRef.current?.focus({ preventScroll: true }),
                   );
                 }}
               />
